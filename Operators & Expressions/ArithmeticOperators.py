17a794312deb8df9522478bfa9c7093dc808cd1e
vishal Paswan <vishalgkp222@gmail.com>
2026-03-29 11:36:16 +0530

    Operators

---
+++ Operators & Expressions/ArithmeticOperators.py
@@ -0,0 +1,25 @@
+a = 10
+b = 4
+
+
+# Addition
+print("# Addition", a +b)
+
+# subtraction
+print("# subtraction", a -b)
+
+# multiplication
+print("# multiplication", a * b)
+
+
+# true division (flot)
+print("# true division", a / b)
+
+# floor division 
+print("# floor division ", a // b)
+
+# modulo 
+print("# modulo ", a % b)
+
+# exponentiation 
+print("# exponentiation ", a ** b)
---
+++ Operators & Expressions/Identity&Membership.py
@@ -0,0 +1,6 @@
+a = [1, 2, 3]
+b = a
+
+print(a is b) #true
+print(7 in a) #False
+print(5 not in a) #v true
