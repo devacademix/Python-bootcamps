3d41ca35efca65b5cec33a002a78a23d503ed3a2
vishal Paswan <vishalgkp222@gmail.com>
2026-03-29 11:20:08 +0530

    Variabble_and_dataType

---
+++ Variables & Data Types/01.py
@@ -0,0 +1,12 @@
+# variable_name = value
+name = 'vishal' #sting
+age = 18 #int
+pi = 3.1458  #flot
+is_student = True #boolen
+Nothing = None #nothing 
+
+print(type(name))
+print(type(age))
+print(type(pi))
+print(type(is_student))
+print(type(Nothing))
---
+++ Variables & Data Types/02.py
@@ -0,0 +1,20 @@
+# Task 1
+X =10
+y= 10.5
+z = "10"
+
+# Task 2
+a=5
+b =2.5
+print(a+b)
+print(type(a+b))
+
+# what conect is used hear ?
+x = y = z =100
+print(x,y,z)
+
+a =10 
+b = 20
+# swap a and b
+
+a = [1,2,3]
